5a5032d3f451b9d59c21bd3142c200ceeb2ef4d6
yunoac <francois@dataquest.io>
2022-01-29 22:14:25 +0000

    Show current page

--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "./Container";
 import ContactModal from "./ContactModal";
+import { useLocation } from 'react-router-dom'
+
 
 function Nav(props) {
 
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
- 
   return (
     <div className="container nav-container">
       <ContactModal open={open} handleClose={() => setOpen(false)}/>
@@ -16,11 +18,14 @@ function Nav(props) {
       <div className="f2 nav-name pointer" onClick={() => navigate('/')}>YEYEH CHEN</div>
 
       <div className="nav-buttons">
-        <div className="f1 button z2" onClick={() => navigate('/about')}>ABOUT</div>
 
-        <div className="f1 button z2" onClick={() => navigate('/work')}>WORK</div>
+        <div className={"f1 button z2" + (location.pathname === "/" ? " active" : "")} onClick={() => navigate('/')}>HOME</div>
 
-        <div className="f1 button z2" onClick={() => navigate('contact')}>CONTACT</div>
+        <div className={"f1 button z2" + (location.pathname === "/about" ? " active" : "")} onClick={() => navigate('/about')}>ABOUT</div>
+
+        <div className={"f1 button z2" + (location.pathname === "/work" ? " active" : "")} onClick={() => navigate('/work')}>WORK</div>
+
+        <div className={"f1 button z2" + (location.pathname === "/contact" ? " active" : "")} onClick={() => navigate('contact')}>CONTACT</div>
 
       </div>
     </div>
